c6f4b503efac497c83f34143933d87d18f48ce06
Ante Jurjević <ante.jurjevic@srce.hr>
2019-07-24 12:13:52 +0200

    Parametrize monitoring directory for NFS sensor

--- manifests/agent/nfsclient.pp
+++ manifests/agent/nfsclient.pp
@@ -6,26 +6,22 @@
 class zabbix::agent::nfsclient (
   $options                 = '',
   $dir_zabbix_agentd_confd = $::zabbix::agent::dir_zabbix_agentd_confd,
+  $dir_for_monitoring = $::zabbix::agent::dir_for_monitoring,
 ) inherits zabbix::agent {
 
+    if $dir_for_monitoring {
+      file { "${dir_zabbix_agentd_confd}/nfsclient.conf":
+        ensure  => file,
+        owner   => root,
+        group   => root,
+        mode    => '0644',
+        content => template('zabbix/agent/nfsclient.conf.erb'),
+        notify  => Service['zabbix-agent'],
+      }
+    }
 
-  file { "${dir_zabbix_agentd_confd}/nfsclient.conf" :
-    ensure  => file,
-    owner   => root,
-    group   => root,
-    mode    => '0644',
-    content => template('zabbix/agent/nfsclient.conf.erb'),
-    notify  => Service['zabbix-agent'],
-  }
-
-#  file { "${dir_zabbix_agent_libdir}/proxy.pl" :
-#    ensure  => file,
-#    owner   => root,
-#    group   => root,
-#    mode    => '0755',
-#    source  => 'puppet:///modules/zabbix/agent/proxy.pl',
-#    notify  => Service['zabbix-agent'],
-#    require => ::Sudoers::Allowed_command['zabbix_proxy'],
-#  }
+    else {
+      notify{'!!! zabbix::agent::dir_for_monitoring must be included defined !!!': }
+    }
 
 }
