88d72ba819d8e9903fed56f97edbc364c96b2706
Ante Jurjević <ante.jurjevic@srce.hr>
2019-07-24 11:17:02 +0200

    Parametrize template for MySQL slave agent path

--- manifests/agent/mysql/slave.pp
+++ manifests/agent/mysql/slave.pp
@@ -6,6 +6,7 @@
 class zabbix::agent::mysql::slave (
   $options                 = '',
   $dir_zabbix_agentd_confd = $::zabbix::agent::dir_zabbix_agentd_confd,
+  $dir_zabbix_agent_libdir = $::zabbix::agent::dir_zabbix_agent_libdir,
 ) inherits zabbix::agent {
 
   package { 'zabbix-agent_mysql-slave':
